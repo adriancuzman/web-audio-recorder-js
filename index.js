ba62cf7e6ee229c82cf3eeb13a95a486ad9446c0
Adrian Cuzman <adriancuzman@gmail.com>
2020-08-08 18:14:02 +0300

    made a npm package

---
+++ index.js
@@ -0,0 +1,10 @@
+require('./lib/WebAudioRecorder');
+require('./lib/WebAudioRecorderMp3');
+require('./lib/WebAudioRecorderOgg');
+require('./lib/WebAudioRecorderWav');
+
+require('./lib/Mp3LameEncoder.min');
+require('./lib/OggVorbisEncoder.min');
+require('./lib/WavAudioEncoder.min');
+
+module.exports = 'WebAudioRecorder';
